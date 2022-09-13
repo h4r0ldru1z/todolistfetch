cc709aa4dccb42bfd474495091cf18f2855f5839
mauriciio89 <maugalvez89@gmail.com>
2022-09-13 03:04:37 +0000

    todo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [toDo, setToDo] = useState([]);
+  const [tarea, setTarea] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setToDo((prev) => [...prev, tarea]);
+	setTarea("");
+  };
+  console.log(toDo);
+  const elementDellete = (dIndex)=> {
+	setToDo(toDo.filter((e,i)=>i!=dIndex))
+  }
+
+  return (
+    <div className="container">
+      <div>
+        <h1 className="text-center">ToDo's</h1>
+        <form onSubmit={onSubmit}>
+          <div class="input-group d-flex justify-content-center">
+            <input type="text" onChange={(e) => setTarea(e.target.value)} />
+          </div>
+          <div>
+            {toDo.map((element, dIndex) => {
+              return (
+                <div className="d-flex justify-content-between my-3 col-4 mx-auto">
+                  <p>{element}</p>
+                  <button type="button" class="btn btn-primary" onClick={()=>elementDellete(dIndex)}>
+                    borrar
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
+
+
+
+
